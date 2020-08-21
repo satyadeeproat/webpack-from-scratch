76ed2746946f2861a41344c5aecf8977f08f3e92
Satyadeep <satyadeep.roat@gmail.com>
2020-08-21 13:17:52 +0530

    add commonjs export and es6 export together

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import nav from './nav';
+import {top, bottom} from './footer';
+import makeButton from './button';
 
 console.log(nav());
+console.log(top, bottom);
+console.log(makeButton('primary'));
